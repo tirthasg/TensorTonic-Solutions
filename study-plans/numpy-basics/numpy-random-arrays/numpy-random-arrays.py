17449e06d5ff2e77ef42955b1da33b9eeb9b9f36
Tirtha Sarathi Ghosh <67009804+tirthasg@users.noreply.github.com>
2026-05-01 13:46:10 +0530

    Update solution for numpy-random-arrays

--- study-plans/numpy-basics/numpy-random-arrays/numpy-random-arrays.py
+++ study-plans/numpy-basics/numpy-random-arrays/numpy-random-arrays.py
@@ -5,8 +5,4 @@ def generate_random_array(shape, kind, seed):
     Returns: 2D ndarray of float64 random values
     """
     np.random.seed(seed)
-
-    if kind == "uniform":
-        return np.random.random(shape)
-
-    return np.random.standard_normal(shape)
+    return np.random.random(shape) if kind == "uniform" else np.random.standard_normal(shape)
