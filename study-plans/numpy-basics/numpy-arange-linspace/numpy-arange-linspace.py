42b2d7ee4b850f371ce3091b1e41ff97420ca257
Tirtha Sarathi Ghosh <67009804+tirthasg@users.noreply.github.com>
2026-05-01 13:37:45 +0530

    Update solution for numpy-arange-linspace

--- study-plans/numpy-basics/numpy-arange-linspace/numpy-arange-linspace.py
+++ study-plans/numpy-basics/numpy-arange-linspace/numpy-arange-linspace.py
@@ -12,11 +12,12 @@ def create_sequence(start, stop, param, kind):
             dtype=np.float64,
         )
         if kind == "arange"
-        else np.linspace(
+        else
+        np.linspace(
             start=start,
             stop=stop,
             num=param,
             endpoint=True,
-            dtype=np.float64,
+            dtype=np.float64
         )
     )
